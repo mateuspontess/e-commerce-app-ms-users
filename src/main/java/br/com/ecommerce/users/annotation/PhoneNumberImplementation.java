2fde07014fe972286a584b3b3ed71c0f20265266
Mateus Pontes <mateuspsdd@gmail.com>
2024-05-13 16:43:57 -0300

    Add @PhoneNumberValidator annotation

---
+++ src/main/java/br/com/ecommerce/users/annotation/PhoneNumberImplementation.java
@@ -0,0 +1,29 @@
+package br.com.ecommerce.users.annotation;
+
+import com.google.i18n.phonenumbers.NumberParseException;
+import com.google.i18n.phonenumbers.PhoneNumberUtil;
+import com.google.i18n.phonenumbers.Phonenumber.PhoneNumber;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+public class PhoneNumberImplementation implements ConstraintValidator<PhoneNumberValidator, String>{
+	
+	@Override
+	public void initialize(PhoneNumberValidator constraintAnnotation) {}
+	
+	@Override
+	public boolean isValid(String phoneNumber, ConstraintValidatorContext context) {
+		PhoneNumberUtil pnUtil = PhoneNumberUtil.getInstance();
+		PhoneNumber pn;
+		
+		try {
+			pn = pnUtil.parse(phoneNumber, "BR");
+			
+		} catch (NumberParseException e) {
+			return false;
+		}
+		
+		return pnUtil.isValidNumberForRegion(pn, "BR");
+	}
+}
---
+++ src/main/java/br/com/ecommerce/users/annotation/PhoneNumberValidator.java
@@ -0,0 +1,20 @@
+package br.com.ecommerce.users.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = PhoneNumberImplementation.class)
+public @interface PhoneNumberValidator {
+	
+    String message() default "Invalid phone number.";
+	String countryCode() default "BR";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+}
