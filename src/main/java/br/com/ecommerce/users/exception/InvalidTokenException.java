9a0c146a67bee8c9dd49468f58a5ad9c699ce0e3
Mateus Pontes <mateuspsdd@gmail.com>
2024-05-16 09:23:06 -0300

    Add `InvalidTokenException` class

---
+++ src/main/java/br/com/ecommerce/users/exception/InvalidTokenException.java
@@ -0,0 +1,9 @@
+package br.com.ecommerce.users.exception;
+
+public class InvalidTokenException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+
+	public InvalidTokenException(String message) {
+		super(message == "" || message == null ? "Invalid token" : message);
+	}
+}
