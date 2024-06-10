72306d9bc1b5fe034632cceef1571442de43066e
Mateus Pontes <mateuspsdd@gmail.com>
2024-06-10 09:25:11 -0300

    test: add `UserControllerUnitTest`

---
+++ src/test/java/br/com/ecommerce/users/unit/UserControllerUnitTest.java
@@ -0,0 +1,85 @@
+package br.com.ecommerce.users.unit;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoInteractions;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.io.IOException;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.json.JacksonTester;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import br.com.ecommerce.users.controller.UserController;
+import br.com.ecommerce.users.model.Address;
+import br.com.ecommerce.users.model.AddressDTO;
+import br.com.ecommerce.users.model.User;
+import br.com.ecommerce.users.model.UserUpdateDTO;
+import br.com.ecommerce.users.service.UserService;
+
+@WebMvcTest(UserController.class)
+@AutoConfigureJsonTesters
+public class UserControllerUnitTest {
+	
+	@MockBean
+	private UserService service;
+	
+	@Autowired
+	private MockMvc mvc;
+
+	@Autowired
+	private JacksonTester<UserUpdateDTO> userUpdateDTOJson;
+
+	@Test
+	void updateUserTest01() throws IOException, Exception {
+		// arrange
+		UserUpdateDTO requestBody = new UserUpdateDTO("update name", "update@email.com", null, new AddressDTO());
+		
+		User userMock = User.builder().name("user").email("user@email.com").phone_number("(11) 11111-1111)").address(new Address()).build();
+		when(service.getUserByToken(any())).thenReturn(userMock);
+		doNothing().when(service).updateUser(requestBody, userMock);
+
+		// act and assert
+		mvc.perform(
+			MockMvcRequestBuilders.put("/users")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(userUpdateDTOJson.write(requestBody).getJson())
+				.header("Authorization", "Bearer token")
+			)
+			.andExpect(status().isOk())
+			.andReturn().getResponse();
+
+		verify(service).getUserByToken(anyString());
+		verify(service).updateUser(any(), any());
+	}
+	
+	@Test
+	void updateUserTest02() throws IOException, Exception {
+		// arrange
+		UserUpdateDTO requestBody = new UserUpdateDTO("update name", "update@email.com", null, new AddressDTO());
+		
+		User userMock = User.builder().name("user").email("user@email.com").phone_number("(11) 11111-1111)").address(new Address()).build();
+		when(service.getUserByToken(any())).thenReturn(userMock);
+		doNothing().when(service).updateUser(requestBody, userMock);
+
+		// act and assert
+		mvc.perform(
+			MockMvcRequestBuilders.put("/users")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(userUpdateDTOJson.write(requestBody).getJson())
+			)
+			.andExpect(status().isBadRequest());
+
+		verifyNoInteractions(service);
+	}
+}
