d7cffd06e8024b6938f828d55eedb8c9969714d8
Mateus Pontes <mateuspsdd@gmail.com>
2024-06-20 15:39:21 -0300

    refactor: update imports

--- src/main/java/br/com/ecommerce/users/controller/UserController.java
+++ src/main/java/br/com/ecommerce/users/controller/UserController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.ecommerce.users.model.User;
 import br.com.ecommerce.users.model.UserIdAndRoleDTO;
 import br.com.ecommerce.users.model.UserResponseDTO;
 import br.com.ecommerce.users.model.UserUpdateDTO;
